4a8c6b01d29ebc950cc7f0da92f01204fb0b71c4
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-25 04:10:56 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,34 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species, :cats, :dogs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+    @cats = []
+    @dogs = []
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.size
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def cats
+    
+  end
+
 end
